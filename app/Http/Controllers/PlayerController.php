cb181bbd117170396188f7f57eeb19bf07cac839
longchamp0505 <longchamp0505@gmail.com>
2025-06-23 13:58:39 +0900

    選手詳細画面にダイレクトアクセスされた際は選手一覧画面に遷移する機能を追加

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -9,6 +9,8 @@ class PlayerController extends Controller
 {
     public function index()
     {
+        // 一覧画面から来たことを記録
+        session(['from_list' => true]); 
         // 1ページに20件ずつ取得（ページネーション）
         $players = Player::paginate(20);
 
@@ -17,6 +19,10 @@ class PlayerController extends Controller
 
     public function show($id)
     {
+        // 一覧画面から来ていない場合はリダイレクト
+        if (!session()->pull('from_list')) {
+            return redirect()->route('players.index');
+        }
         $player = Player::findOrFail($id);
         return view('players.show', compact('player'));
     }
